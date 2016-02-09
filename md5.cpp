4234f516e57645abc24fa7bbf543ee473af88804
Taylor Peterson <TaylorP913@Gmail.com>
2016-02-08 23:40:13 -0700

    Searching benchmark Worksing

--- md5.cpp
+++ md5.cpp
@@ -1,4 +1,4 @@
-//
+﻿//
 //  main.cpp
 //  md5_project
 //
@@ -13,44 +13,69 @@
 #include "md5.h"
 #include <time.h>
 
-using namespace std;
+//using namespace std;
 
 union Guess guess;
+union Guess output;
+union Guess input;
+
+#define input1 0x453e41d218e071cc//zzzzzz
+#define input2 0xfb2d1c99ce23906a
+
+//#define input1 0x0b4e7a0e5fe84ad3
+//#define input2 0x5fb5f95b9ceeac79
 
 int main(int argc, char *argv[]) {
-    uint64_t start = Digit5;//Digit5 is the start of the 6 digit space
-	uint64_t end = Digit5+2000000;//Digit6 is end of 6 digit space
+	uint64_t start = Digit6-1000000;//Digit5 is the start of the 6 digit space
+	uint64_t end = Digit6;//Digit6 is end of 6 digit space
 	uint64_t hashes = end - start;
-	unsigned char hash[16];
-    //unsigned char pass[] = "abcdefghijklmnopqrstuvwxyz";
-    // Hash is: c3fcd3d76192e4007dfb496cca67e13b
-    
-    clock_t start_time, final_time;
-    start_time = clock();
-
-    MD5 md5;
-    //md5_initialize(&md5);
-
-    //for(uint64_t i = Digit5; i < Digit6; i++){//full test
-    for(uint64_t i = start; i < end; i++){//short test
-        increment_pass(&guess, i);
-        //guess_print(&guess);
-        //md5_set(&md5,pass,strlen((char*)pass));
-        //md5_pad(&md5);
-        //md5_initialize(&md5);
-        md5_manipulate(&md5,&guess);
-        md5_bigendian(&md5,hash);
-        //md5_print(hash);
-    }
+	uint64_t cmp1, cmp2;
+	//unsigned char hash[16];
+	//unsigned char pass[] = "abcdefghijklmnopqrstuvwxyz";
+	// Hash is: c3fcd3d76192e4007dfb496cca67e13b
+	transform_input(input1, input2);
 
 
+	clock_t start_time, final_time;
+	start_time = clock();
 
-    final_time = clock();
-    //had to multiply by ten since the system had an incorect CLOCKS_PER_SEC value, possibly due to virtual machine
-	printf("%i hashes\n", hashes);
-	//printf("%i clocks per sec\n", CLOCKS_PER_SEC);
-	printf("%f seconds\n",((float)difftime(final_time,start_time))/CLOCKS_PER_SEC);
-	printf("%f hashes/seconds\n", hashes/(((float)difftime(final_time, start_time)) / CLOCKS_PER_SEC));
+	MD5 md5;
+	//md5_initialize(&md5);
 
-    return 0;
+	for (uint64_t i = start; i < end; i++) {
+		increment_pass(&guess, i);
+		//guess_print(&guess);
+		//md5_set(&md5,pass,strlen((char*)pass));
+		//md5_pad(&md5);
+		//md5_initialize(&md5);
+		md5_manipulate(&output, &guess);
+		//md5_bigendian(&output,hash);
+		//md5_print(hash);
+
+		
+		cmp1 = output._64[0] ^ input._64[0];
+		cmp2 = output._64[1] ^ input._64[1];
+		
+		//printf("  %i\n", (char *)output._8);
+		//printf("  %i\n", (char *)input._8);
+		
+		if ((cmp1 | cmp2) == 0)
+		{
+			printf("Found the hash!\n");
+			printf("  %s\n", (char *)guess._8);
+			//exit(1);
+		}
+
+	}
+
+
+
+	final_time = clock();
+	//had to multiply by ten since the system had an incorect CLOCKS_PER_SEC value, possibly due to virtual machine
+	printf("%f seconds\n", ((float)difftime(final_time, start_time)) / CLOCKS_PER_SEC);
+	printf("%f hashes/seconds\n", hashes / (((float)difftime(final_time, start_time)) / CLOCKS_PER_SEC));
+
+
+
+	return 0;
 }
--- md5.h
+++ md5.h
@@ -1,4 +1,4 @@
-//
+﻿//
 //  md5.h
 //  md5_project
 //
@@ -18,27 +18,27 @@
 //constructore for input data type
 union Guess
 {
-    uint8_t   _8[64];
-    uint32_t  _32[16];
-    uint64_t  _64[8];
+	uint8_t   _8[64];
+	uint32_t  _32[16];
+	uint64_t  _64[8];
 
-    Guess() // initialization
-    {
-        memset(&this->_8[0], 0, 64);
-    };
+	Guess() // initialization
+	{
+		memset(&this->_8[0], 0, 64);
+	};
 };
 #define alph(a) ('a'+a)
 void guess_print(union Guess *guess) {
-    int idx;
-    printf("%c",guess->_8[0]);
-    printf("%c",guess->_8[1]);
-    printf("%c",guess->_8[2]);
-    printf("%c",guess->_8[3]);
-    printf("%c",guess->_8[4]);
-    printf("%c",guess->_8[5]);
-    printf("%c",guess->_8[6]);
-    printf("%c",guess->_8[7]);
-    printf("\n");
+	int idx;
+	printf("%c", guess->_8[0]);
+	printf("%c", guess->_8[1]);
+	printf("%c", guess->_8[2]);
+	printf("%c", guess->_8[3]);
+	printf("%c", guess->_8[4]);
+	printf("%c", guess->_8[5]);
+	printf("%c", guess->_8[6]);
+	printf("%c", guess->_8[7]);
+	printf("\n");
 }
 //The increment value for each alpha character in the password 1-26
 #define  I1 (uint64_t) 26
@@ -53,18 +53,34 @@ void guess_print(union Guess *guess) {
 
 void increment_pass(union Guess *guess, uint64_t i)
 {
-        guess->_8[0] = alph((i-Digit5) % I1);
-        guess->_8[1] = alph((((i-Digit5)/I1) % I1));
-        guess->_8[2] = alph((((i-Digit5)/I2) % I1));
-        guess->_8[3] = alph((((i-Digit5)/I3) % I1));
-        guess->_8[4] = alph((((i-Digit5)/I4) % I1));
-        guess->_8[5] = alph((((i-Digit5)/I5) % I1));
-        guess->_8[6] = 0x80;
-        memset(&guess->_8[7], 0, 56); //Pad
-        guess->_64[7] = 6 * 8; //length * 8 
+	guess->_8[0] = alph((i - Digit5) % I1);
+	guess->_8[1] = alph((((i - Digit5) / I1) % I1));
+	guess->_8[2] = alph((((i - Digit5) / I2) % I1));
+	guess->_8[3] = alph((((i - Digit5) / I3) % I1));
+	guess->_8[4] = alph((((i - Digit5) / I4) % I1));
+	guess->_8[5] = alph((((i - Digit5) / I5) % I1));
+	guess->_8[6] = 0x80;
+	memset(&guess->_8[7], 0, 56); //Pad
+	guess->_64[7] = 6 * 8; //length * 8 
 }
 
-
+#define transform_input( a,  b) {\
+        input._8[0] = (a>> 56) &0xff;\
+        input._8[1] = (a>> 48) &0xff;\
+        input._8[2] = (a>> 40) &0xff;\
+        input._8[3] = (a>> 32) &0xff;\
+        input._8[4] = (a>> 24) &0xff;\
+        input._8[5] = (a>> 16) &0xff;\
+        input._8[6] = (a>>  8) &0xff;\
+        input._8[7] = (a     ) &0xff;\
+        input._8[8] = (b>> 56) &0xff;\
+        input._8[9] = (b>> 48) &0xff;\
+        input._8[10] = (b>> 40) &0xff;\
+        input._8[11] = (b>> 32) &0xff;\
+        input._8[12] = (b>> 24) &0xff;\
+        input._8[13] = (b>> 16) &0xff;\
+        input._8[14] = (b>>  8) &0xff;\
+        input._8[15] = (b     ) &0xff;}
 
 #define alpha 26;
 
@@ -122,221 +138,223 @@ a = (b + a); \
 //};
 
 uint32_t T[64] = {
-    0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee,
-    0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
-    0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be,
-    0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821,
-    
-    0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa,
-    0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8,
-    0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed,
-    0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a,
-    
-    0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c,
-    0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70,
-    0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05,
-    0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665,
-    
-    0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039,
-    0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1,
-    0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1,
-    0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391,
+	0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee,
+	0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
+	0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be,
+	0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821,
+
+	0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa,
+	0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8,
+	0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed,
+	0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a,
+
+	0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c,
+	0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70,
+	0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05,
+	0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665,
+
+	0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039,
+	0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1,
+	0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1,
+	0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391,
 };
 
 // All the 's' values for the 4 rounds
 int S[64] = {
-    7, 12, 17, 22,  7, 12, 17, 22,
-    7, 12, 17, 22,  7, 12, 17, 22,
-    
-    5, 9, 14, 20,  5, 9, 14, 20,
-    5, 9, 14, 20,  5, 9, 14, 20,
-    
-    4, 11, 16, 23,  4, 11, 16, 23,
-    4, 11, 16, 23,  4, 11, 16, 23,
-    
-    6, 10, 15, 21,  6, 10, 15, 21,
-    6, 10, 15, 21,  6, 10, 15, 21
+	7, 12, 17, 22,  7, 12, 17, 22,
+	7, 12, 17, 22,  7, 12, 17, 22,
+
+	5, 9, 14, 20,  5, 9, 14, 20,
+	5, 9, 14, 20,  5, 9, 14, 20,
+
+	4, 11, 16, 23,  4, 11, 16, 23,
+	4, 11, 16, 23,  4, 11, 16, 23,
+
+	6, 10, 15, 21,  6, 10, 15, 21,
+	6, 10, 15, 21,  6, 10, 15, 21
 };
 
 //uint32_t blocks[16];
 
 typedef struct {
-    unsigned char message[64];      //data
-    // unsigned int msg[16];
-    unsigned int msg_len;           //msg_len
-    unsigned int blocks[4];         //blocks
-    unsigned int msgbitlen[2];      //bitlen
+	unsigned char message[64];      //data
+									// unsigned int msg[16];
+	unsigned int msg_len;           //msg_len
+	unsigned int blocks[4];         //blocks
+	unsigned int msgbitlen[2];      //bitlen
 } MD5;
 
 void md5_initialize(MD5 *md5)
 {
-    md5 -> msg_len = 0;
-    md5 -> blocks[0] = ai;
-    md5 -> blocks[1] = bi;
-    md5 -> blocks[2] = ci;
-    md5 -> blocks[3] = di;
-    md5 -> msgbitlen[0] = 0;
-    md5 -> msgbitlen[1] = 0;
-    
-    // for(unsigned int i = 0; i < 16; i++){
-    //  md5 -> msg[i] = 0;
-    // }
+	md5->msg_len = 0;
+	md5->blocks[0] = ai;
+	md5->blocks[1] = bi;
+	md5->blocks[2] = ci;
+	md5->blocks[3] = di;
+	md5->msgbitlen[0] = 0;
+	md5->msgbitlen[1] = 0;
+
+	// for(unsigned int i = 0; i < 16; i++){
+	//  md5 -> msg[i] = 0;
+	// }
 }
 
 void md5_set(MD5 *md5, unsigned char message[], unsigned long length) {
-    unsigned int i;
-    
-    for (i = 0; i < length; i++) {
-        md5 -> message[md5 -> msg_len] = message[i];
-        md5 -> msg_len++;
-        /* if (md5 -> msg_len == 64) {
-         md5_transform(md5,md5 -> message);
-         }*/
-    }
+	unsigned int i;
+
+	for (i = 0; i < length; i++) {
+		md5->message[md5->msg_len] = message[i];
+		md5->msg_len++;
+		/* if (md5 -> msg_len == 64) {
+		md5_transform(md5,md5 -> message);
+		}*/
+	}
 }
 
 
-void md5_manipulate(MD5 *md5,union Guess *gss) {
-    //unsigned char message[] = {guess->_8[0],guess->_8[1],guess->_8[2],guess->_8[3],guess->_8[4],guess->_8[5],guess->_8[6],guess->_8[7]};
-    unsigned int a,b,c,d;
-    //unsigned int msg[16];
-    unsigned int i,j;
-    
-    // // For loop changes from big endian to little endian
-    // for (i = 0, j = 0; i < 16; i++, j += 4) {
-    //     msg[i] = (message[j]) + (message[j + 1] << 8) + (message[j + 2] << 16) + (message[j + 3] << 24);
-    // }
-    
-    // Store all initial values of a,b,c,d into their respective variables for manipulation
-    a = ai;
-    b = bi;
-    c = ci;
-    d = di;
-    
-    FF(a, b, c, d, gss->_32[0],  S[0],  T[0]);
-    FF(d, a, b, c, gss->_32[1],  S[1],  T[1]);
-    FF(c, d, a, b, gss->_32[2],  S[2],  T[2]);
-    FF(b, c, d, a, gss->_32[3],  S[3],  T[3]);
-    FF(a, b, c, d, gss->_32[4],  S[4],  T[4]);
-    FF(d, a, b, c, gss->_32[5],  S[5],  T[5]);
-    FF(c, d, a, b, gss->_32[6],  S[6],  T[6]);
-    FF(b, c, d, a, gss->_32[7],  S[7],  T[7]);
-    FF(a, b, c, d, gss->_32[8],  S[8],  T[8]);
-    FF(d, a, b, c, gss->_32[9],  S[9],  T[9]);
-    FF(c, d, a, b, gss->_32[10], S[10], T[10]);
-    FF(b, c, d, a, gss->_32[11], S[11], T[11]);
-    FF(a, b, c, d, gss->_32[12], S[12], T[12]);
-    FF(d, a, b, c, gss->_32[13], S[13], T[13]);
-    FF(c, d, a, b, gss->_32[14], S[14], T[14]);
-    FF(b, c, d, a, gss->_32[15], S[15], T[15]);
-    
-    GG(a, b, c, d, gss->_32[1],  S[16], T[16]);
-    GG(d, a, b, c, gss->_32[6],  S[17], T[17]);
-    GG(c, d, a, b, gss->_32[11], S[18], T[18]);
-    GG(b, c, d, a, gss->_32[0],  S[19], T[19]);
-    GG(a, b, c, d, gss->_32[5],  S[20], T[20]);
-    GG(d, a, b, c, gss->_32[10], S[21], T[21]);
-    GG(c, d, a, b, gss->_32[15], S[22], T[22]);
-    GG(b, c, d, a, gss->_32[4],  S[23], T[23]);
-    GG(a, b, c, d, gss->_32[9],  S[24], T[24]);
-    GG(d, a, b, c, gss->_32[14], S[25], T[25]);
-    GG(c, d, a, b, gss->_32[3],  S[26], T[26]);
-    GG(b, c, d, a, gss->_32[8],  S[27], T[27]);
-    GG(a, b, c, d, gss->_32[13], S[28], T[28]);
-    GG(d, a, b, c, gss->_32[2],  S[29], T[29]);
-    GG(c, d, a, b, gss->_32[7],  S[30], T[30]);
-    GG(b, c, d, a, gss->_32[12], S[31], T[31]);
-    
-    HH(a, b, c, d, gss->_32[5],  S[32], T[32]);
-    HH(d, a, b, c, gss->_32[8],  S[33], T[33]);
-    HH(c, d, a, b, gss->_32[11], S[34], T[34]);
-    HH(b, c, d, a, gss->_32[14], S[35], T[35]);
-    HH(a, b, c, d, gss->_32[1],  S[36], T[36]);
-    HH(d, a, b, c, gss->_32[4],  S[37], T[37]);
-    HH(c, d, a, b, gss->_32[7],  S[38], T[38]);
-    HH(b, c, d, a, gss->_32[10], S[39], T[39]);
-    HH(a, b, c, d, gss->_32[13], S[40], T[40]);
-    HH(d, a, b, c, gss->_32[0],  S[41], T[41]);
-    HH(c, d, a, b, gss->_32[3],  S[42], T[42]);
-    HH(b, c, d, a, gss->_32[6],  S[43], T[43]);
-    HH(a, b, c, d, gss->_32[9],  S[44], T[44]);
-    HH(d, a, b, c, gss->_32[12], S[45], T[45]);
-    HH(c, d, a, b, gss->_32[15], S[46], T[46]);
-    HH(b, c, d, a, gss->_32[2],  S[47], T[47]);
-    
-    II(a, b, c, d, gss->_32[0],  S[48], T[48]);
-    II(d, a, b, c, gss->_32[7],  S[49], T[49]);
-    II(c, d, a, b, gss->_32[14], S[50], T[50]);
-    II(b, c, d, a, gss->_32[5],  S[51], T[51]);
-    II(a, b, c, d, gss->_32[12], S[52], T[52]);
-    II(d, a, b, c, gss->_32[3],  S[53], T[53]);
-    II(c, d, a, b, gss->_32[10], S[54], T[54]);
-    II(b, c, d, a, gss->_32[1],  S[55], T[55]);
-    II(a, b, c, d, gss->_32[8],  S[56], T[56]);
-    II(d, a, b, c, gss->_32[15], S[57], T[57]);
-    II(c, d, a, b, gss->_32[6],  S[58], T[58]);
-    II(b, c, d, a, gss->_32[13], S[59], T[59]);
-    II(a, b, c, d, gss->_32[4],  S[60], T[60]);
-    II(d, a, b, c, gss->_32[11], S[61], T[61]);
-    II(c, d, a, b, gss->_32[2],  S[62], T[62]);
-    II(b, c, d, a, gss->_32[9],  S[63], T[63]);
-    
-    //md5->blocks[0] += a;
-    //md5->blocks[1] += b;
-    //md5->blocks[2] += c;
-    //md5->blocks[3] += d;
+void md5_manipulate(union Guess *md5, union Guess *gss) {
+	//unsigned char message[] = {guess->_8[0],guess->_8[1],guess->_8[2],guess->_8[3],guess->_8[4],guess->_8[5],guess->_8[6],guess->_8[7]};
+	unsigned int a, b, c, d;
+	//unsigned int msg[16];
+	unsigned int i, j;
+
+	// // For loop changes from big endian to little endian
+	// for (i = 0, j = 0; i < 16; i++, j += 4) {
+	//     msg[i] = (message[j]) + (message[j + 1] << 8) + (message[j + 2] << 16) + (message[j + 3] << 24);
+	// }
+
+	// Store all initial values of a,b,c,d into their respective variables for manipulation
+	a = ai;
+	b = bi;
+	c = ci;
+	d = di;
+
+	FF(a, b, c, d, gss->_32[0], S[0], T[0]);
+	FF(d, a, b, c, gss->_32[1], S[1], T[1]);
+	FF(c, d, a, b, gss->_32[2], S[2], T[2]);
+	FF(b, c, d, a, gss->_32[3], S[3], T[3]);
+	FF(a, b, c, d, gss->_32[4], S[4], T[4]);
+	FF(d, a, b, c, gss->_32[5], S[5], T[5]);
+	FF(c, d, a, b, gss->_32[6], S[6], T[6]);
+	FF(b, c, d, a, gss->_32[7], S[7], T[7]);
+	FF(a, b, c, d, gss->_32[8], S[8], T[8]);
+	FF(d, a, b, c, gss->_32[9], S[9], T[9]);
+	FF(c, d, a, b, gss->_32[10], S[10], T[10]);
+	FF(b, c, d, a, gss->_32[11], S[11], T[11]);
+	FF(a, b, c, d, gss->_32[12], S[12], T[12]);
+	FF(d, a, b, c, gss->_32[13], S[13], T[13]);
+	FF(c, d, a, b, gss->_32[14], S[14], T[14]);
+	FF(b, c, d, a, gss->_32[15], S[15], T[15]);
+
+	GG(a, b, c, d, gss->_32[1], S[16], T[16]);
+	GG(d, a, b, c, gss->_32[6], S[17], T[17]);
+	GG(c, d, a, b, gss->_32[11], S[18], T[18]);
+	GG(b, c, d, a, gss->_32[0], S[19], T[19]);
+	GG(a, b, c, d, gss->_32[5], S[20], T[20]);
+	GG(d, a, b, c, gss->_32[10], S[21], T[21]);
+	GG(c, d, a, b, gss->_32[15], S[22], T[22]);
+	GG(b, c, d, a, gss->_32[4], S[23], T[23]);
+	GG(a, b, c, d, gss->_32[9], S[24], T[24]);
+	GG(d, a, b, c, gss->_32[14], S[25], T[25]);
+	GG(c, d, a, b, gss->_32[3], S[26], T[26]);
+	GG(b, c, d, a, gss->_32[8], S[27], T[27]);
+	GG(a, b, c, d, gss->_32[13], S[28], T[28]);
+	GG(d, a, b, c, gss->_32[2], S[29], T[29]);
+	GG(c, d, a, b, gss->_32[7], S[30], T[30]);
+	GG(b, c, d, a, gss->_32[12], S[31], T[31]);
+
+	HH(a, b, c, d, gss->_32[5], S[32], T[32]);
+	HH(d, a, b, c, gss->_32[8], S[33], T[33]);
+	HH(c, d, a, b, gss->_32[11], S[34], T[34]);
+	HH(b, c, d, a, gss->_32[14], S[35], T[35]);
+	HH(a, b, c, d, gss->_32[1], S[36], T[36]);
+	HH(d, a, b, c, gss->_32[4], S[37], T[37]);
+	HH(c, d, a, b, gss->_32[7], S[38], T[38]);
+	HH(b, c, d, a, gss->_32[10], S[39], T[39]);
+	HH(a, b, c, d, gss->_32[13], S[40], T[40]);
+	HH(d, a, b, c, gss->_32[0], S[41], T[41]);
+	HH(c, d, a, b, gss->_32[3], S[42], T[42]);
+	HH(b, c, d, a, gss->_32[6], S[43], T[43]);
+	HH(a, b, c, d, gss->_32[9], S[44], T[44]);
+	HH(d, a, b, c, gss->_32[12], S[45], T[45]);
+	HH(c, d, a, b, gss->_32[15], S[46], T[46]);
+	HH(b, c, d, a, gss->_32[2], S[47], T[47]);
+
+	II(a, b, c, d, gss->_32[0], S[48], T[48]);
+	II(d, a, b, c, gss->_32[7], S[49], T[49]);
+	II(c, d, a, b, gss->_32[14], S[50], T[50]);
+	II(b, c, d, a, gss->_32[5], S[51], T[51]);
+	II(a, b, c, d, gss->_32[12], S[52], T[52]);
+	II(d, a, b, c, gss->_32[3], S[53], T[53]);
+	II(c, d, a, b, gss->_32[10], S[54], T[54]);
+	II(b, c, d, a, gss->_32[1], S[55], T[55]);
+	II(a, b, c, d, gss->_32[8], S[56], T[56]);
+	II(d, a, b, c, gss->_32[15], S[57], T[57]);
+	II(c, d, a, b, gss->_32[6], S[58], T[58]);
+	II(b, c, d, a, gss->_32[13], S[59], T[59]);
+	II(a, b, c, d, gss->_32[4], S[60], T[60]);
+	II(d, a, b, c, gss->_32[11], S[61], T[61]);
+	II(c, d, a, b, gss->_32[2], S[62], T[62]);
+	II(b, c, d, a, gss->_32[9], S[63], T[63]);
+
+	md5->_32[0] = ai+a;
+	md5->_32[1] = bi+b;
+	md5->_32[2] = ci+c;
+	md5->_32[3] = di+d;
+
+
 }
 
 void md5_pad(MD5 *md5) {
-    unsigned int i;
-    
-    i = md5 -> msg_len;
-    md5 -> message[i++] = 0x80;
-    
-    while (i < 64) {
-        md5 -> message[i++] = 0x00;
-    }
-    md5 -> message[56] = md5 -> msg_len * 8;
-    
-    // md5_manipulate(md5,md5 -> message);
-    
-    /*    else if (md5 -> msg_len >= 56) {
-     md5 -> message[i++] = 0x80;
-     while (i < 64)
-     md5 -> message[i++] = 0x00;
-     md5_manipulate(md5,md5 -> message);
-     memset(md5 -> message,0,56);
-     }*/
+	unsigned int i;
+
+	i = md5->msg_len;
+	md5->message[i++] = 0x80;
+
+	while (i < 64) {
+		md5->message[i++] = 0x00;
+	}
+	md5->message[56] = md5->msg_len * 8;
+
+	// md5_manipulate(md5,md5 -> message);
+
+	/*    else if (md5 -> msg_len >= 56) {
+	md5 -> message[i++] = 0x80;
+	while (i < 64)
+	md5 -> message[i++] = 0x00;
+	md5_manipulate(md5,md5 -> message);
+	memset(md5 -> message,0,56);
+	}*/
 }
 
-void md5_bigendian(MD5 *md5, unsigned char final[]) {
-    
-    unsigned int m[4],n[4],o[4],p[4];
-    unsigned int i,j;
-    
-    for(i = 0, j = 0; i < 4; i++, j += 4){
-        m[i] = md5 -> blocks[i];
-        n[i] = md5 -> blocks[i];
-        o[i] = md5 -> blocks[i];
-        p[i] = md5 -> blocks[i];
-        final[j] = (m[i] & 0x000000ff);
-        final[j+1] = ((n[i] >> 8) & 0x0000ff);
-        final[j+2] = ((o[i] >> 16) & 0x00ff);
-        final[j+3] = ((p[i] >> 24) & 0xff);
-    }
-    /*    for(i = 0; i < 4; i++){
-     hash[i] = ((ctx->blocks[i] & 0x000000ff) << 24) +
-     ((ctx->blocks[i] & 0x0000ff00) << 8) +
-     ((ctx->blocks[i] & 0x00ff0000) >> 8) +
-     ((ctx->blocks[i] & 0xff000000) >> 24);
-     }*/
-}
+//void md5_bigendian(union guess *md5, unsigned char final[]) {
+//
+//	unsigned int m[4], n[4], o[4], p[4];
+//	unsigned int i, j;
+//
+//	for (i = 0, j = 0; i < 4; i++, j += 4) {
+//		m[i] = md5._32[i];
+//		n[i] = md5._32[i];
+//		o[i] = md5._32[i];
+//		p[i] = md5._32[i];
+//		final[j] = (m[i] & 0x000000ff);
+//		final[j + 1] = ((n[i] >> 8) & 0x0000ff);
+//		final[j + 2] = ((o[i] >> 16) & 0x00ff);
+//		final[j + 3] = ((p[i] >> 24) & 0xff);
+//	}
+//	/*    for(i = 0; i < 4; i++){
+//	hash[i] = ((ctx->blocks[i] & 0x000000ff) << 24) +
+//	((ctx->blocks[i] & 0x0000ff00) << 8) +
+//	((ctx->blocks[i] & 0x00ff0000) >> 8) +
+//	((ctx->blocks[i] & 0xff000000) >> 24);
+//	}*/
+//}
 
 void md5_print(unsigned char final[]) {
-    int idx;
-    for (idx=0; idx < 16; idx++)
-        printf("%02x",final[idx]);
-    printf("\n");
+	int idx;
+	for (idx = 0; idx < 16; idx++)
+		printf("%02x", final[idx]);
+	printf("\n");
 }
 
 
